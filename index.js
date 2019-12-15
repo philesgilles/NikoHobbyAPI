9572eaff604720ea6ca2f1b72d42d7660e6d26c9
Philippe Gilles <philgdep@gmail.com>
2019-12-15 10:50:23 +0100

    fix tie week end

--- index.js
+++ index.js
@@ -47,8 +47,9 @@ client = mqtt.connect(config.mqtt.url, config.mqtt.options);
         const time = new Date();
         const hours = time.getHours();
         //const minutes = time.getMinutes();
-        //const day = time.getDay();
-        if (hours < 5 || hours > 18) {
+        const day = time.getDay();
+        console.log(day, hours);
+        if ((hours < 5 || hours > 18) || ((day === 6 || day === 0) && (hours >= 10 && hours < 15 ))) {
           if (payload.temp < 18.5) {
             console.log(printDate + " [Too Cold] -- Switch ON heating !");
             let { topic, data } = setNewTemp(
